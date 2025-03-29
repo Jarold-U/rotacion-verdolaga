7e0f45f53c54a86d06475af766ac3c96fee472a8
Jarold-U <jusugam@poligran.edu.co>
2025-03-28 19:40:19 -0500

    logo

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title="Sistema de Rotación Verdolaga", layout="centered
 # Encabezado con logo
 st.markdown("""
     <div style='text-align: center;'>
-        <img src='https://atlnacional.com.co/wp-content/uploads/2021/05/Solo-escudo.png' width='130'>
+        <img src='https://dimayor.com.co/wp-content/uploads/2024/06/Atletico-nacional.png' width='130'>
         <h1 style='color:#008D52;'>Sistema de Rotación Verdolaga</h1>
     </div>
 """, unsafe_allow_html=True)
